b5f84eccefa2a55a5a2098817bcf63153d00cb30
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-09-23 14:49:13 +0300

    intial commit

---
+++ testWPF/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace testWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string API_KEY = "07d1480f18fa6b47d8b43d4817eb60b3";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetWeatherButton_Click(object sender, RoutedEventArgs e)
+        {
+            string city = UserCityTextBox.Text.Trim();
+            if(city.Length < 2)
+            {
+                MessageBox.Show("Укажите ваш город");
+                return;
+            }
+           
+            try
+            {
+               string data = await GetWeather(city);
+                var json = JObject.Parse(data);
+                string temp = json["main"]["temp"].ToString();
+                WeatherResults.Content = temp;
+
+            }
+            catch(HttpRequestException ex)
+            {
+                MessageBox.Show("Укажите верный город");
+                WeatherResults.Content = "";
+            }
+
+            
+        }
+
+        private async Task<string> GetWeather(string city)
+        {
+            HttpClient client = new HttpClient();
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric";
+            string response = await client.GetStringAsync(url);
+            return response;
+        }
+    }
+}
